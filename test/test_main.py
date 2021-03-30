f65aaa5c9af1618c0785ff1de10a599c3aae9304
Michele Pangrazzi <michele@wonderflow.co>
2021-03-30 17:02:39 +0200

    test(core): add test for step 3

--- test/test_main.py
+++ test/test_main.py
@@ -20,8 +20,10 @@ def test_parse_single_sentence():
 
 
 def test_parse_multiple_sentences():
-    text = "14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" \
+    text = (
+        "14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n"
         "14:26:15 Agent : Aliquam non cursus erat, ut blandit lectus."
+    )
 
     sentences = parser(text)
 
@@ -36,6 +38,44 @@ def test_parse_multiple_sentences():
             "date": "14:26:15",
             "mention": "14:26:15 Agent : ",
             "sentence": "Aliquam non cursus erat, ut blandit lectus.",
+            "type": "agent",
+        },
+    ]
+
+
+def test_full_chat():
+    text = (
+        "14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n"
+        "14:26:15 Agent : Aliquam non cursus erat, ut blandit lectus.\n"
+        "14:27:00 Customer : Pellentesque cursus maximus felis, pharetra porta purus aliquet viverra.\n"
+        "14:27:47 Agent : Vestibulum tempor diam eu leo molestie eleifend."
+    )
+
+    sentences = parser(text)
+
+    assert sentences == [
+        {
+            "date": "14:24:32",
+            "mention": "14:24:32 Customer : ",
+            "sentence": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n",
+            "type": "customer",
+        },
+        {
+            "date": "14:26:15",
+            "mention": "14:26:15 Agent : ",
+            "sentence": "Aliquam non cursus erat, ut blandit lectus.\n",
+            "type": "agent",
+        },
+        {
+            "date": "14:27:00",
+            "mention": "14:27:00 Customer : ",
+            "sentence": "Pellentesque cursus maximus felis, pharetra porta purus aliquet viverra.\n",
+            "type": "customer",
+        },
+        {
+            "date": "14:27:47",
+            "mention": "14:27:47 Agent : ",
+            "sentence": "Vestibulum tempor diam eu leo molestie eleifend.",
             "type": "agent",
         },
     ]
