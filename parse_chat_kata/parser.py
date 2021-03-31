219d97ef79ff9284a76d91ed9d323c6ff34bd4ee
Michele Pangrazzi <michele@wonderflow.co>
2021-03-31 22:53:32 +0200

    refactor(parser): refactor

--- parse_chat_kata/parser.py
+++ parse_chat_kata/parser.py
@@ -2,31 +2,37 @@ import re
 
 DATE_REGEX = r"\d{2}:\d{2}:\d{2}"
 NAME_REGEX = r"\w+"
-DATE_NAME_REGEX = re.compile(r"(%s) (%s :?\s?)" % (DATE_REGEX, NAME_REGEX))
+DATE_NAME_REGEX = re.compile(r"^(%s) (%s :?\s?)" % (DATE_REGEX, NAME_REGEX))
+
+global customer_name
 
 
 def parse(text: str):
-    return [parse_sentence(sent) for sent in get_sentences(text) if sent]
+    return [parse_sentence(i, sent) for i, sent in enumerate(get_sentences(text))]
 
 
 def get_sentences(text: str):
-    return (
+    sents = (
         text.splitlines(True)
         if re.search("\n+", text)
-        else re.split(r"(?=%s %s :?)" % (DATE_REGEX, NAME_REGEX), text)
+        else re.split(r"(?=%s %s)" % (DATE_REGEX, NAME_REGEX), text)
     )
 
+    return filter(None, sents)
 
-def parse_sentence(sent: str):
+
+def parse_sentence(i: int, sent: str):
+    global customer_name
+
     match = DATE_NAME_REGEX.match(sent)
-
     date, name = match.groups()
-    sep = match.span()[1]
-    sentence = sent[sep:]
+
+    if i == 0:
+        customer_name = name
 
     return {
         "date": date,
         "mention": f"{date} {name}",
-        "sentence": sentence,
-        "type": "customer" if "Customer" in name else "agent",
+        "sentence": sent[match.span()[1] :],
+        "type": "customer" if name == customer_name else "agent",
     }
